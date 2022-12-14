d46bd1029efc73499f5adc18e14204b4feeda589
Ahkar Toe Maw <ahkartoemaw@acedatasystems.com>
2022-12-14 12:06:47 +0630

    hangfire

---
+++ HangfireApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Hangfire;
+using HangfireApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HangfireTest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HangfireTestPost()
+        {
+            string cronExpression = _configuration.GetSection("ScheduleCronExpression").Value;
+
+            RecurringJob.AddOrUpdate(() => Fun(), cronExpression);
+            return View("HangfireTest");
+        }
+
+        public static void Fun()
+        {
+            Console.WriteLine("Recurring!");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
